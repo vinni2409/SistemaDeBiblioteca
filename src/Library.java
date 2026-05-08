d086e4fd08c45cf19b4bbe05436a242007635b48
vinni2409 <viniciusfurtado230@gmail.com>
2026-05-07 21:19:23 -0300

    Create Class Library

---
+++ src/Library.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+
+public class Library {
+    private ArrayList<Book> BookList;
+
+    public Library() {
+        BookList = new ArrayList<>();
+    }
+
+    public void AddBook(Book book) {
+        if (searchBook(book.getIsbn()) != null) {
+            System.out.println("A book with ISBN " + book.getIsbn() + " already exists.");
+        }
+    }
+
+    public void ListBook(){
+        if(BookList.isEmpty()){
+            System.out.println("No books in library");
+        }else{
+            for(Book book : BookList){
+                book.showInfo();
+                System.out.println("......");
+            }
+        }
+    }
+
+    public Book searchBook(String isbn) {
+        for (Book book : BookList) {
+            if (book.getIsbn().equals(isbn)) {
+                return book;
+            }
+        }
+        return null;
+    }
+
+    public void removeBook(String isbn){
+        Book book = searchBook(isbn);
+        if(book != null){
+            BookList.remove(book);
+            System.out.println("Book / " + book.getTitle() + "/ removed successfuly.");
+        }else{
+            System.out.println("Book with ISBN " + isbn + " not found.");
+        }
+    }
+
+    public void lendBook(String isbn){
+        Book book = searchBook(isbn);
+        if (book != null) {
+            book.lendBook();
+        } else {
+            System.out.println("Book with ISBN " + isbn + " not found.");
+        }
+    }
+
+    public void returnBook(String isbn){
+        Book book = searchBook(isbn);
+        if (book != null) {
+            book.returnBook();
+        } else {
+            System.out.println("Book with ISBN " + isbn + " not found.");
+        }
+    }
+}
